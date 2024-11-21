35eb02585afe1eceaada2b4b6564ce711b53b8c9
farihafari <fareehajabeen62@gmail.com>
2024-11-21 04:06:15 -0800

    js

---
+++ webdesigining/assets/js/script.js
@@ -0,0 +1,8 @@
+let firstName="ali";
+let lastName="khan";
+let Qualification= "MSCS";
+
+
+document.write( "<h1 style='color:blue'>my name is "+firstName+"</h1><h1> my Qualification is "+Qualification+"</h1>");
+ document.write(`<h1 style="color:red">my father name is ${lastName}</h1><h2>${Qualification}<h2>`)
+// document.write(x)
