321fbf08d7b30235e545b077fa3eea52e852ba59
farihafari <fareehajabeen62@gmail.com>
2024-11-28 16:57:55 +0500

    conditional statements

---
+++ webdesigining/assets/js/conditions.js
@@ -0,0 +1,17 @@
+// let uinput = prompt("enter your name");
+
+// if(uinput){
+//     alert("dear! "+uinput+" welcome to our website"); //statement block, decision 
+// }else{
+//     alert("please fill the input first");
+//     location.assign("conditions.html")
+
+// }
+
+let uinput = prompt("Enter Your Age");
+
+if( uinput >= 25 && uinput <=40 ){
+    alert("Your Application Has Been Submitted")
+}else{
+    alert("You Are Not Capable")
+}
