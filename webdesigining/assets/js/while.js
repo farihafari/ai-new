c6bd62583b27e5d3f631f86d7fc9f4075591a832
farihafari <fareehajabeen62@gmail.com>
2024-12-14 15:48:45 +0500

    recover

---
+++ webdesigining/assets/js/while.js
@@ -0,0 +1,26 @@
+let inital = prompt("enter initial number to start loop");
+let final  = prompt("enter terminatin value of loof");
+
+let d=document.getElementById("data");
+
+if(inital && final){
+    if(!isNaN(inital && final)){
+       if(final>inital){
+while(inital<=final){
+    inital++;
+d.innerHTML+=`<p>your loop iteration count is ${inital}</p>`;
+// document.write(`<p>your loop iteration count is ${inital}</p>`)    
+
+}
+       }else{
+        alert("termination value must be greater then initial");
+        location.assign("while.html")
+       }
+    }else{
+        alert("input must be in number format");
+        location.assign("while.html")
+    }
+}else{
+    alert("every field is required to fill");
+    location.assign("while.html")
+}
