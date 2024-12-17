4fd3cee53f5ae52852427eae90b1ce18f4543bbe
farihafari <fareehajabeen62@gmail.com>
2024-12-17 17:02:07 +0500

    array done

---
+++ webdesigining/assets/js/array2d.js
@@ -0,0 +1,20 @@
+let std_detail =[
+    ['alishar','cpism','9 to 11',7600],
+    ['aliya','cpism','9 to 11',7600,],
+    ['alisha','cpism','9 to 11',7600],
+    ['alisba','cpism','9 to 11',7600],
+    ['aliyan','cpism','9 to 11',7600],
+    ['ali raza','cpism','9 to 11',7600],
+    ['ali hamaza','cpism','9 to 11',7600],
+    ['alishar','cpism','9 to 11',7600],
+    ['ali','cpism','9 to 11',7600],
+    ['ali','cpism','9 to 11',7600]
+
+]
+// document.write(std_detail[2][0])
+for(let i =0;i<std_detail.length;i++){
+    document.write("<br>");
+    for(let j=0;j<std_detail[i].length;j++){
+        document.write(std_detail[i][j]+" ")
+    }
+}
